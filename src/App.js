03dfb7ec065c021eae96853115a32892c69888fb
sharadaabakar <76987090+sharadaabakar@users.noreply.github.com>
2021-04-24 19:47:06 +0530

    files uploaded
    added

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css';
+import Header from "./components/Header";
+import About from "./components/About";
+import Shop from "./components/Shop";
+import Menu from "./components/Menu";
+import Clients from "./components/Clients";
+
+function App() {
+  return (
+    <div>
+   <Header />
+   <About />
+   <Shop />
+   <Menu />
+   <Clients />
+    </div>
+  );
+}
+
+export default App;
